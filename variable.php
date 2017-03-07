addda58baf44334f833760881412e805ab11ac3f
Extreeeme <martindcontact@gmail.com>
2017-03-07 13:53:21 +0100

    Exercice 2 DONE

--- variable.php
+++ variable.php
@@ -1,4 +1,23 @@
-<?php
-	$nom = "Martin";
-	echo $nom;
-?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Starting_PHP</title>
+		<link rel="stylesheet" href="style/css/style.css">
+	</head>
+	<body>
+		<?php
+			//Exercice 1
+			$nom = "Martin";
+			echo "<p>".$nom."</p>";
+			//Exercice 2
+			$nom = "HULEU";
+			$prenom = "Eddy";
+			$age = 4;
+			echo "<p>". $nom . " ". $prenom ." " . $age ."</p>";
+		?>
+	</body>
+</html>
