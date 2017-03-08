561ce2651d25111f6dd3a4498cc00c5ec805d64a
Extreeeme <martindcontact@gmail.com>
2017-03-08 15:34:56 +0100

    Ex 7.1 DONE

--- formulaire.php
+++ formulaire.php
@@ -8,12 +8,15 @@
 		<link rel="stylesheet" href="style/css/style.css">
 	</head>
 	<body>
-		<a href="variable.php">Exercice 1</a>
-		<a href="condition.php">Exercice 2</a>
-		<a href="boucle.php">Exercice 3</a>
-		<a href="fonction.php">Exercice 4</a>
-		<a href="tableau.php">Exercice 5</a>
-		<a href="parametre.php">Exercice 6</a>
-		<a href="formulaire.php">Exercice 7</a>
+		<div id="no_scroll">
+		<div class="ex ">
+		<form action="user.php" methode="get">
+			<input type="text" name ="nom" placeholder="Nom">
+			<input type="text" name ="prenom" placeholder="Prénom">
+			<button type="submit">VALIDE FRERE</button>
+		</form>
+		</div>
+		</div>
+
 	</body>
 </html>
