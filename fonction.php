2e9c99d993381b2431c4299a44e5720e7cdae9a7
Extreeeme <martindcontact@gmail.com>
2017-03-08 11:25:23 +0100

    Ex 4.3 DONE

--- fonction.php
+++ fonction.php
@@ -19,10 +19,42 @@
 			if(true()){
 				echo "<p id='koukou'>TEST OK</p>";
 			}else{
-				echo "<p>ERREUR</p>";			}
+				echo "<p>ERREUR</p>";
+			}
 		?>
 		</div>
 		</div>
-
+		<div id="no_scroll">
+		<div class="ex ">
+		<?php
+			//Exercice 2
+			echo "<h3>EXERCICE 2</h3>";
+			function koko(string $koukou){
+				return $koukou;
+			}
+			if(koko("TEST OK") == "TEST OK"){
+				echo "<p id='koukou'>TEST OK</p>";
+			}else{
+				echo "<p>ERREUR</p>";
+			}
+		?>
+		</div>
+		</div>
+		<div id="no_scroll">
+		<div class="ex ">
+		<?php
+			//Exercice 3
+			echo "<h3>EXERCICE 3</h3>";
+			function eh(string $azerty, string $aze){
+				return $azerty.$aze;
+			}
+			if(eh("1","2") == "12"){
+				echo "<p id='koukou'>TEST OK</p>";
+			}else{
+				echo "<p>ERREUR</p>";
+			}
+		?>
+		</div>
+		</div>
 	</body>
 </html>
